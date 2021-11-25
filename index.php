5a0909856f1aaaaae1ad72bbc2a19d1e11b1a7d8
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-11-25 16:11:34 +0900

    修正

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
 <?php
+require_once("task.php");
 require_once("tasklist.php");
 
-$tasks = new TaskList();
-$data = $tasks->getAllTask();
-var_dump($data);
+//データ取得
+//$tasks = new TaskList();
+//$data = $tasks->getAllTask();
+//var_dump($data);
+
+// データ登録
+$task = new Task("テスト2", "2021/11/29", false);
+$task->create();
+
+
 ?>
