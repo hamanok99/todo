67d5fdebf7326b2bb7a2f493d68965ff088053a9
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-12-03 16:08:30 +0900

    修正

--- index.php
+++ index.php
@@ -8,13 +8,23 @@ require_once("tasklist.php");
 //var_dump($data);
 
 // データ登録
-$task = new Task("テスト2", "2021/11/29", false);
-$result = $task->create();
-
-if ($result) {
-    echo "登録成功";
-} else {
-    echo "登録失敗";
-}
+//$task = new Task("テスト2", "2021/11/29", false);
+//$result = $task->create();
 
 ?>
+
+<script type="text/javascript">
+    function clickMe(){
+    <?php $task = new Task("テスト2", "2021/11/29", false); ?>
+    var result ="<?php $task.create(); ?>";
+    document.write(result);
+}
+</script>
+
+<html>
+    <body>
+        <form>
+            <button onclick="clickMe()"> Click </button>
+        </form>
+    </body>
+</html>
