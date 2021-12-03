108d56056af9e668d5908bc26b75696abc3d92fb
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-12-03 12:06:10 +0900

    修正

--- task.php
+++ task.php
@@ -30,6 +30,8 @@ class Task {
         {
             if (is_null($this->id))
             {
+                echo $this->fix_flg;
+                echo var_export($this->fix_flg);
                 $sql = sprintf("INSERT INTO todo(name, deadline, fix_flg) VALUES('%s', '%s', %s);", $this->name, $this->deadline, var_export($this->fix_flg));
                 $url = parse_url(getenv('DATABASE_URL'));
                 $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
