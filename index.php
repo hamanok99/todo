284f4907f129de01622a2b9c962512c5417288b1
hamanok99 <kazuki.hamano@wisdom.co.jp>
2021-12-06 09:39:31 +0900

    更新

--- index.php
+++ index.php
@@ -1,24 +1,13 @@
 <?php
 require_once("task.php");
-require_once("tasklist.php");
-
-function insert()
-{
-    if(!empty($_POST)){
-        $task = new Task($_POST['name'], $_POST['deadline'], false);
-        return $task->create();
-    } else {
-        return false;
-    }
-}
 ?>
 
 <script type="text/javascript">
-window.onload = function() {
-
+window.onload = function()
+{
     <?php if ($_POST['name']) : ?>
-        var name = <?php $_POST['name']; ?>;
-        var result ="<?php insert(); ?>";
+        <?php $task = new Task($_POST['name'], $_POST['deadline'], false); ?>
+        var result ="<?php $task->create(); ?>";
         if (result){
             alert('登録完了');
         }
